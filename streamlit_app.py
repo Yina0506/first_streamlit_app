e9eefc26fcbac802ff1d3c8e4c1a31863e047176
Yina0506 <123548226+Yina0506@users.noreply.github.com>
2023-03-16 16:04:32 +0000

    Update streamlit_app.py
    correct

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit
 streamlit.title('My parents new heathy diner')
 
 streamlit.header('Breakfast menu')
-streamlit.text(🥣  'Strawberry oat bowl')
-streamlit.text(🥗 🥑'Green smoothie')
-streamlit.text(🐔🍞'Chicken pie')
+streamlit.text('🥣  Strawberry oat bowl')
+streamlit.text('🥗 🥑Green smoothie')
+streamlit.text('🐔🍞Chicken pie')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
