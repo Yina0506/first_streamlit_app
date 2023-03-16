e3a32ee7280996b0dfe655903099385ce127b721
Yina0506 <123548226+Yina0506@users.noreply.github.com>
2023-03-16 16:03:54 +0000

    Update streamlit_app.py
    Add emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,11 @@ import streamlit
 streamlit.title('My parents new heathy diner')
 
 streamlit.header('Breakfast menu')
-streamlit.text('Strawberry oat bowl')
-streamlit.text('Green smoothie')
-streamlit.text('Salmon Sushi')
+streamlit.text(🥣  'Strawberry oat bowl')
+streamlit.text(🥗 🥑'Green smoothie')
+streamlit.text(🐔🍞'Chicken pie')
 
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 
