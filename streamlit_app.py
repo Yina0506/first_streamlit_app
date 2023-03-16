757fb94fa7153edef5512272f19d8627c45a5c94
Yina0506 <123548226+Yina0506@users.noreply.github.com>
2023-03-16 16:00:58 +0000

    Update streamlit_app.py
    Update header and texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,20 @@
 import streamlit
+
 streamlit.title('My parents new heathy diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Strawberry oat bowl')
+streamlit.text('Green smoothie')
+streamlit.text('Salmon Sushi')
+
+
+
+
+
+
+
+
+
+
+
+
